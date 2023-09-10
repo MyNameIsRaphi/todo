2ba1615d79d0076b02aaf668875409599ec6dd44
Raphael Käser <raphaelkaeser05@gmail.com>
2023-09-10 12:17:53 +0200

    Added error display functionality on register page. Next step is to check enered data with backend

--- public/register.js
+++ public/register.js
@@ -1,18 +1,59 @@
-let confirmInput = $(".confirmedPassword");
+let confirmInput = $("#inputPassword4");
 
-let newPassword = $(".newPassword")
+let newPassword = $("#inputPassword5")
 
 let submitButton = $("#signin")
 
-let noMatchError = $(".noMatchError")
+let noMatchError = $("#noMatchError")
+
+let noEmailError = $("#noEmailError")
+
+let noPasswordError = $("#noPasswordError")
+
+
+let email = $(".enteredEmail")
 
 submitButton.click(
     (event) => {
-        
+        unDisplayAllErrors()
+        let validEmail = (validateEmail(email.val()))
+
+        console.log(validEmail)
+
         let password = newPassword.val();
         let confirmedPassword = confirmInput.val();
-        if (password !== confirmedPassword){
+        if (password != confirmedPassword) {
             console.log("Passwords don't match")
+            
+            displayError(noMatchError)
+
+        } 
+        else if (!validEmail){
+            displayError(noEmailError)
+        } else if (password.length < 8){
+            displayError(noPasswordError)
         }
     }
 )
+
+function displayError(error) {
+    error.css("display", "flex")
+    error.css("justifyContent", "center")
+    error.css("alignItems", "center")
+}
+
+function unDisplayAllErrors(){
+     unDisplayError(noEmailError)
+     unDisplayError(noMatchError)
+     
+     unDisplayError(noPasswordError)
+}
+function unDisplayError(error){
+    error.css("display", "none")
+}
+
+function validateEmail(email) {
+    var regex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+    return regex.test(email);
+  }
+  
